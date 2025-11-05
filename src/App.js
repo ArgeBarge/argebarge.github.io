c371b48faf59dac91b50b9abf5cb87fd9a7501d7
psyjb21 <psyjb21@nottingham.ac.uk>
2025-11-05 14:44:00 +0000

    feat: add multiple pages

--- src/App.js
+++ src/App.js
@@ -1,73 +1,69 @@
 import './App.css';
-import project1 from './tower_defence.png'
-import project2 from './graphics_scene.png'
-import project3_1 from './app_1.png'
-import project3_2 from './app_2.png'
+import Projects from './pages/projects/Projects.js'
 
-function MyButton({className, string}) {
+import { NavLink, Routes, Route } from 'react-router-dom'
+
+export default function App() {
   return (
-    <button className={className}>{string}</button>
-  )
-}
-
-function ProjectContainer({imageList, title, description}) {
-
-
-  return (
-      <div className="ProjectContainer">
-        <h2>{title}</h2>
-        <div className="ProjectImage">
-          {imageList.map((image) => (
-            <img src={image} />
-          ))}
-        </div>
-        
-        <div className='ProjectDescription'>
-          <p>{description}</p>
-        </div>
-      </div>
-    )
-
-}
-
-export default function MyApp() {
-  return (
-    <div className="App">
-      <h1>Hello, I'm James!</h1>
-      <p>
-        I'm a recent graduate from the University of Nottingham
-      </p>
-        <h1>Projects</h1>
-        <ProjectContainer 
-          imageList={[]}
-          title={"This website"} 
-          description={"You're looking at it!"}>
-        </ProjectContainer>
-
-        <ProjectContainer 
-          imageList={[project1]} 
-          title={"Tower defence game"} 
-          description={"Created a tower defence game in C++ using a custom framework built off the SDL C library."}>
-        </ProjectContainer>
-
-        <ProjectContainer
-          imageList={[project2]}
-          title={"Graphics scene"}
-          description={"Created a scene in OpenGL applying graphical techniques such as lighting, shadows and cameras."}
-        >
-        </ProjectContainer>
-
-        <ProjectContainer
-          imageList={[project3_1, project3_2]}
-          title={"Music player app"}
-          description={
-            "Created a music app in android studio using the android SDK. It was able to read\
-            audio files from a folder and allowed you to play/pause music. It also had some basic settings."
-          }
-        >
-
-        </ProjectContainer>
-       
+    <div className='app'>
+      <h1>React Router Demo</h1>
+      <Navigation />
+      <Main />
     </div>
   );
+  
+}
+
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink className={({isActive}) => isActive ? "current" : ""} to='/'>Home</NavLink></li>
+        <li><NavLink className={({isActive}) => isActive ? "current" : ""} to='/about'>About</NavLink></li>
+        <li><NavLink className={({isActive}) => isActive ? "current" : ""} to='/contact'>Contact</NavLink></li>
+        <li><NavLink className={({isActive}) => isActive ? "current" : ""} to='/projects'>Projects</NavLink></li>
+      </ul>
+    </nav>
+  );
+}
+function Home() {
+  return (
+    <div className='home'>
+      <h1>Welcome to my portfolio website</h1>
+      <p> Feel free to browse around.</p>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div className='about'>
+      <h1>About Me</h1>
+      <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+      <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+    </div>
+  );
+  
+
+}
+
+function Contact() {
+  return (
+    <div className='contact'>
+      <h1>Contact Me</h1>
+      <p>You can reach me via email: <strong>hello@example.com</strong></p>
+    </div>
+  );
+}
+
+function Main() {
+  return (
+    <Routes>
+      <Route path='\' Component={Home}></Route>
+      <Route path='/about' Component={About}></Route>
+      <Route path='/contact' Component={Contact}></Route>
+      <Route path='/projects' Component={Projects}></Route>
+    </Routes>
+  );
+  
 }
