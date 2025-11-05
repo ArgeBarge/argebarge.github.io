c709d8bb82c8780689daaef8614e5efd61d42098
psyjb21 <psyjb21@nottingham.ac.uk>
2025-11-05 11:30:53 +0000

    add projects to website|

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import project1 from './tower_defence.png'
+import project2 from './graphics_scene.png'
+import project3_1 from './app_1.png'
+import project3_2 from './app_2.png'
 
-function App() {
+function MyButton({className, string}) {
+  return (
+    <button className={className}>{string}</button>
+  )
+}
+
+function ProjectContainer({imageList, title, description}) {
+
+
+  return (
+      <div className="ProjectContainer">
+        <h2>{title}</h2>
+        <div className="ProjectImage">
+          {imageList.map((image) => (
+            <img src={image} />
+          ))}
+        </div>
+        
+        <div className='ProjectDescription'>
+          <p>{description}</p>
+        </div>
+      </div>
+    )
+
+}
+
+export default function MyApp() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Hello, I'm James!</h1>
+      <p>
+        I'm a recent graduate from the University of Nottingham
+      </p>
+        <h1>Projects</h1>
+        <ProjectContainer 
+          imageList={[]}
+          title={"This website"} 
+          description={"You're looking at it!"}>
+        </ProjectContainer>
+
+        <ProjectContainer 
+          imageList={[project1]} 
+          title={"Tower defence game"} 
+          description={"Created a tower defence game in C++ using a custom framework built off the SDL C library."}>
+        </ProjectContainer>
+
+        <ProjectContainer
+          imageList={[project2]}
+          title={"Graphics scene"}
+          description={"Created a scene in OpenGL applying graphical techniques such as lighting, shadows and cameras."}
         >
-          Learn React
-        </a>
-      </header>
+        </ProjectContainer>
+
+        <ProjectContainer
+          imageList={[project3_1, project3_2]}
+          title={"Music player app"}
+          description={
+            "Created a music app in android studio using the android SDK. It was able to read\
+            audio files from a folder and allowed you to play/pause music. It also had some basic settings."
+          }
+        >
+
+        </ProjectContainer>
+       
     </div>
   );
 }
-
-export default App;
